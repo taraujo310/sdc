0e8831769596fa53f4e6ffb70533f3b0064a24ab
Thiago Ururay <thiago.araujo@dtmtec.com.br>
2017-10-31 21:06:55 -0200

    parsing db_url

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$dbopts = parse_url(getenv('DATABASE_URL'));
 return [
 
     /*
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DATABASE_URL', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'sdcbrasil'),
-            'username' => env('DB_USERNAME', 'sdcbrasil'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $dbopts["host"],
+            'port' => $dbopts["port"],
+            'database' => ltrim($dbopts["path"],'/'),
+            'username' => $dbopts["user"],
+            'password' => $dbopts["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
