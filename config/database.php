e56ab206f289f72c504c17336e3542427fb64142
Thiago Ururay <thiago.araujo@dtmtec.com.br>
2017-10-31 21:03:39 -0200

    using database_url config var

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DATABASE_URL', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'sdcbrasil'),
             'username' => env('DB_USERNAME', 'sdcbrasil'),
